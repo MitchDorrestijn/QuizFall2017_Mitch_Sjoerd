7f2f408675e4bb4b30d77078eb4c918da2a2d1e2
ixnas <sjoerd@b00n.org>
2017-10-25 13:19:48 +0200

    Added bodyParser, implemented POST /api/games/:gameId/teams

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 let randomstring = require ("randomstring");
 let express = require ("express");
+let bodyParser = require ("body-parser");
 let mongoose = require ("mongoose");
 let questions = require ("./schema/questions.js").model;
 let games = require ("./schema/games.js").model;
+let teams = require ("./schema/teams.js").model;
 let cors = require ("cors");
 
 let port = 8080;
@@ -24,6 +26,8 @@ mongoose.Promise = global.Promise;
 app.use (cors ({
 	origin: '*'
 }));
+
+app.use (bodyParser.json ());
 
 // QuizzMaster
 app.post ("/api/games", (req, res) => {
@@ -83,6 +87,72 @@ app.get ("/api/categories", (req, res) => {
 	questions.find ().distinct ('category', (err, categories) => {
 		res.json (categories);
 	});
+});
+
+// QuizApp
+app.post ("/api/games/:gameId/teams", (req, res) => {
+	// TODO unit test
+	if (!req.params.gameId) {
+		res.json ({
+			success: false,
+			error: "No gameID specified"
+		});
+	} else if (!req.body.name || req.body.name === "") {
+		res.json ({
+			success: false,
+			error: "No name specified"
+		});
+	} else {
+		let team = new teams ({
+			name: req.body.name,
+			appliedGame: req.params.gameId
+		});
+		let promise = new Promise ((resolve, reject) => {
+			games.findOne ({_id: req.params.gameId}, (err, result) => {
+				if (err) {
+					reject (err.toString ());
+				} else if (result) {
+					resolve ();
+				} else {
+					reject ("Game does not exist");
+				}
+			})
+		}).then (() => {
+			let promise2 = new Promise ((resolve, reject) => {
+				teams.findOne ({appliedGame: req.params.gameId, name: req.body.name}, (err, result) => {
+					if (err) {
+						reject (err.toString ());
+					} else if (result) {
+						reject ("Team has already applied for this game");
+					} else {
+						resolve ();
+					}
+				});
+			});
+			return promise2;
+		}).then (() => {
+			let promise3 = new Promise ((resolve, reject) => {
+				team.save ((err) => {
+					if (err) {
+						reject (err.toString ());
+					} else {
+						resolve ();
+					}
+				});
+			});
+			return promise3;
+		}).then (() => {
+			res.send ({
+				success: true,
+				error: null
+			});
+		}).catch ((err) => {
+			res.send ({
+				success: false,
+				error: err
+			});
+		});
+	}
 });
 
 // Start the server
