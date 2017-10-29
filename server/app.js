f72c7058884d1e0e1aaa5cbcee2754e96d775f6d
ixnas <sjoerd@b00n.org>
2017-10-29 15:42:28 +0100

    GET /api/games/:gameId/rounds/:roundId/answers/current implemented, removed a redundant function

--- server/app.js
+++ server/app.js
@@ -592,6 +592,63 @@ app.put ("/api/games/:gameId/rounds/:roundId/questions/current", (req, res) => {
 	}
 });
 
+app.get ("/api/games/:gameId/rounds/:roundId/answers/current", (req, res) => {
+	if (!req.params.gameId) {
+		res.json ({
+			success: false,
+			error: "No game ID specified"
+		});
+	} else {
+		let promise = gameExists (req.params.gameId)
+			.then ((game) => {
+				return new Promise ((resolve, reject) => {
+					let roundId = parseInt (req.params.roundId, 10);
+					if (game.rounds [roundId]) {
+						let activeAnswer = game.rounds [roundId].activeAnswer;
+						if (activeAnswer !== null) {
+							let result = {};
+							result.answer = null;
+							result.questionId = game.rounds [roundId].answers [activeAnswer].question;
+							result.teamAnswers = [];
+							for (let elem of game.rounds [roundId].answers [activeAnswer].answers) {
+								if (elem.answer !== "") {
+									result.teamAnswers.push (elem);
+								}
+							}
+							resolve (result);
+						} else {
+							reject ("No question is active");
+						}
+					} else {
+						reject ("Round doesn't exist");
+					}
+				});
+			}).then ((result) => {
+				return new Promise ((resolve, reject) => {
+					let questionId = result.questionId;
+					delete result.questionId;
+					questions.findOne ({_id: mongoose.Types.ObjectId (questionId)}, (err, result2) => {
+						if (err) {
+							reject (err.toString ());
+						} else if (result2) {
+							result.answer = result2.answer;
+							resolve (result);
+						} else {
+							reject ("Question not found");
+						}
+					});
+				});
+			}).then ((result) => {
+				res.json (result);
+			}).catch ((err) => {
+				res.json ({
+					success: false,
+					error: err
+				});
+			});
+	}
+});
+
 // QuizzApp
 app.post ("/api/games/:gameId/teams", (req, res) => {
 	// TODO unit test
@@ -715,52 +772,6 @@ app.get ("/api/games/:gameId/rounds/current", (req, res) => {
 				res.json ({
 					activeRound: activeRound
 				});
-			}).catch ((err) => {
-				res.json ({
-					success: false,
-					error: err
-				});
-			});
-	}
-});
-
-app.get ("/api/games/:gameId/rounds/:roundId/questions/current", (req, res) => {
-	if (!req.params.gameId) {
-		res.json ({
-			success: false,
-			error: "No game ID specified"
-		});
-	} else if (!req.params.roundId) {
-		res.json ({
-			success: false,
-			error: "No round ID specified"
-		});
-	} else {
-		let promise = gameExists (req.params.gameId)
-			.then ((game) => {
-				return new Promise ((resolve, reject) => {
-					let result = {};
-					if (game.rounds [parseInt (req.params.roundId, 10)]) {
-						if (game.rounds [parseInt (req.params.roundId, 10)].activeAnswer !== null) {
-							let activeAnswer = game.rounds [parseInt (req.params.roundId)].answers [game.rounds [parseInt (req.params.roundId)].activeAnswer];
-							result.questionId = activeAnswer.question;
-							questions.findOne ({_id: result.questionId}, (err, question) => {
-								if (err) {
-									reject (err.toString ());
-								} else {
-									result.question = question.question;
-									resolve (result);
-								}
-							});
-						} else {
-							reject ("There is no active question");
-						}
-					} else {
-						reject ("The round does not exist");
-					}
-				});
-			}).then ((result) => {
-				res.json (result);
 			}).catch ((err) => {
 				res.json ({
 					success: false,
