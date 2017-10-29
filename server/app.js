639e559c670ca7c83a13a8c8e33b2c1fd034166c
ixnas <sjoerd@b00n.org>
2017-10-29 11:08:38 +0100

    GET /api/games/:gameId/rounds/:roundId/questions implemented

--- server/app.js
+++ server/app.js
@@ -452,6 +452,76 @@ app.post ("/api/games/:gameId/rounds/:roundId/questions", (req, res) => {
 	}
 });
 
+app.get ("/api/games/:gameId/rounds/:roundId/questions", (req, res) => {
+	if (!req.params.gameId) {
+		res.json ({
+			success: false,
+			error: "No game ID specified"
+		});
+	} else if (!req.params.roundId) {
+		res.json ({
+			success: false,
+			error: "No round ID specified"
+		});
+	} else {
+		let promise = gameExists (req.params.gameId)
+			.then ((game) => {
+				return new Promise ((resolve, reject) => {
+					let roundQuestions = [];
+					for (let answer of game.rounds [parseInt (req.params.roundId, 10)].answers) {
+						roundQuestions.push ({
+							questionId : answer.question
+						});
+					}
+					let playedQuestions = game.playedQuestions.map ((elem) => {
+						return elem.toString ();
+					});
+					roundQuestions = roundQuestions.filter (val => !playedQuestions.includes (val.questionId.toString ()));
+					resolve (roundQuestions);
+				});
+			}).then ((roundQuestions) => {
+				return new Promise ((resolve, reject) => {
+					let questionIds = roundQuestions.map ((elem) => {
+						return elem.questionId;
+					});
+					questions.find ({_id: { $in: questionIds }}, (err, result) => {
+						if (err) {
+							reject (err.toString ());
+						} else {
+							let roundQuestionsAndQuestions = [roundQuestions, result];
+							resolve (roundQuestionsAndQuestions);
+						}
+					});
+				});
+			}).then ((roundQuestionsAndQuestions) => {
+				let roundQuestions = roundQuestionsAndQuestions [0];
+				let questions = roundQuestionsAndQuestions [1];
+				let result = [];
+				return new Promise ((resolve, reject) => {
+					for (let i = 0; i < roundQuestions.length; i++) {
+						for (let j = 0; j < questions.length; j++) {
+							if (roundQuestions [i].questionId.toString () === questions [j]._id.toString ()) {
+								result.push ({
+									questionId: roundQuestions [i].questionId,
+									question: questions [j].question,
+									answer: questions [j].answer
+								});
+							}
+						}
+					}
+					resolve (result);
+				});
+			}).then ((result) => {
+				res.json (result);
+			}).catch ((err) => {
+				res.json ({
+					success: false,
+					error: err
+				});
+			});
+	}
+});
+
 // QuizApp
 app.post ("/api/games/:gameId/teams", (req, res) => {
 	// TODO unit test
