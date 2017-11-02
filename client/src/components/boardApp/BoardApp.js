4bf65907e1970e06b2a6ad91c0f677d915e7f46f
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2017-11-02 13:00:02 +0100

    Started implementing the boardapp

--- client/src/components/boardApp/BoardApp.js
+++ client/src/components/boardApp/BoardApp.js
@@ -1,62 +1,76 @@
 import React from 'react';
+import DataAccess from '../../scripts/DataAccess';
 
 export default class BoardApp extends React.Component {
   constructor(props){
     super(props);
     // TODO: GET CURRENT QUESTION, CATEGORY AND ROUNDNUMBER FROM THE DATABASE AND BOOLEANS FROM THE QUIZMASTER.
     this.state = {
-      questionIsClosed: false,
-      showGivenAnswers: false,
-      currentQuestion: "Who wrote the novel Revolutionary Road, which was made into a successful feature film?",
-      currentCategory: "Algemeen",
-      roundNumber: 1,
-      teamStatus: [
-        ['teamName',          true,                 true,                   1,                  10],
-        ['anotherTeamName',   false,                false,                  5,                  10],
-        ['yetanother',        true,                 false,                  8,                  10]
-        //[teamName, teamHasSubmittedAnAnswer, teamHadAcorrectAnswer, amountOfGoodQuestions, roundPoints]
-        //    0                 1                       2                     3                  4
-      ]
+      roomNumber: window.location.pathname.replace("/bord/", ""),
+      result: []
     }
   }
+
+  //
+  // {
+  //  roundNumber: Number,
+  //  questionNumber: Number,
+  //  maxQuestions: Number,
+  //  scores: [{
+  //   team: String,
+  //   correctAnswers: Number,
+  //   roundPoints: Number
+  //  }],
+  //  currentQuestion: {
+  //   name: String,
+  //   category: String,
+  //   closed: Boolean,
+  //   teamsAnswered: [{
+  //    team: String,
+  //    answer: String,
+  //    approved: Boolean
+  //   }]
+  //  }
+  // }
+
   renderTeamInfo(){
-    let teamStatus = this.state.teamStatus.map((teamStatus, i) => {
-      return (
-        <li key={teamStatus[0] + i} className="singleTeam">
-          <h4>{teamStatus[0]}</h4>
-          <ul>
-            <li>Aantal vragen goed: {teamStatus[3]}</li>
-            <li>Aantal roundpoints: {teamStatus[4]}</li>
-          </ul>
-          {teamStatus[1] ? <span className="success">Dit team heeft de huidige vraag zojuist beantwoord</span> : <span className="error">Dit team heeft de huidige vraag nog niet beantwoord</span>}
-          {this.state.showGivenAnswers ?
-            <div>
-              <strong className="rightOrWrong">Dit team had deze vraag als volgd beantwoord:</strong>
-              <span className="answer">[getTeamAnswer]</span>
-              {teamStatus[2] ? <strong className="success">GOED BEANTWOORD!</strong> : <strong className="error">FOUT BEANTWOORD!</strong>}
-            </div>
-            :
-            <div>
-              <strong className="rightOrWrong">Zodra de quizmaster de vraag sluit verschijnt hier het antwoord.</strong>
-            </div>
+    let da = new DataAccess();
+    da.getData(`/games/${this.state.roomNumber}/scores`, (err, res) => {
+      if (err) {
+        console.log (err);
+      } else {
+        let items = [];
+        for (let i = 0; i < res.scores.length; i++) {
+          let subItem;
+          let item;
+          for (let j = 0; j < res.teamAnswers.length; j++) {
+            if (res.scores [i].team === res.teamAnswers[j].team) {
+              if (res.closed) {
+                subItem = (<div><strong className="rightOrWrong">Dit team had deze vraag als volgd beantwoord:</strong><span className={res.teamAnswers[i].approved ? "rightAnswer" : "wrongAnswer"}>{res.teamAnswers[j].answer}</span></div>);
+                  } else {
+                    subItem = (<div><strong className="rightOrWrong">Zodra de quizmaster de vraag sluit verschijnt hier het antwoord.</strong></div>);
+                  }
+                }
+              }
+              item = (<div><h4>{res.scores[i].team}</h4>
+              <ul>
+                <li>{res.scores[i].correctAnswers}</li>
+                <li>{res.scores[i].roundPoints}</li>
+              </ul>
+              {subItem}</div>);
+              items.push (item);
+            }
           }
-        </li>
-      );
-    });
-    return(
-      <ul className="teamScores">
-        {teamStatus}
-      </ul>
-    );
-  }
+          this.setState ({result: items});
+        });
+      }
+
   render(){
     return (
       <div className="scorebord--wrapper intro--header">
         <div className="inner--header">
-          <h1 className="question">{this.state.currentQuestion}</h1>
-          {this.state.questionIsClosed ? <strong className="error questionStatus">De vraag is gesloten. De quizmaster keurt nu uw antwoorden.</strong> : <strong className="success questionStatus">Geef alstublieft nu een antwoord.</strong>}
           {this.renderTeamInfo()}
-          <small className="roundInfo">Dit is ronde <i>{this.state.roundNumber}</i>, deze vraag is afkomstig uit de <i>{this.state.currentCategory}</i> cattegorie.</small>
+          {this.state.result}
         </div>
       </div>
     );
